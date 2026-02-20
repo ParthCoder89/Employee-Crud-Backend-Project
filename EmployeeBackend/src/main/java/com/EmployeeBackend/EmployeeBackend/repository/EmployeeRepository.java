4477c18ee3af629313abb7b60d5ffd40f40395e6
ParthCoder89 <parthgupta5041@gmail.com>
2026-02-20 21:59:55 +0530

    Some Changes in the repository interface of the crud project

--- EmployeeBackend/src/main/java/com/EmployeeBackend/EmployeeBackend/repository/EmployeeRepository.java
+++ EmployeeBackend/src/main/java/com/EmployeeBackend/EmployeeBackend/repository/EmployeeRepository.java
@@ -3,4 +3,5 @@ package com.EmployeeBackend.EmployeeBackend.repository;
 import com.EmployeeBackend.EmployeeBackend.DatabaseData.EmployeeData;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface EmployeeRepository extends JpaRepository<EmployeeData, Long> {
+
 }
